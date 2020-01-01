31d18a5e93a86d1c189ecab91fd1c84dc0399532
Tarun Sunkarneni <tarun.sunkaraneni@gmail.com>
2020-01-01 06:41:32 -0700

    bumped up versions

--- assignments/assignment2/q4_train_atari_linear.py
+++ assignments/assignment2/q4_train_atari_linear.py
@@ -24,20 +24,20 @@ address-ip-of-the-server:6006
 6006 is the default port used by tensorboard.
 """
 if __name__ == '__main__':
-    # make env
-    env = gym.make(config.env_name)
-    env = MaxAndSkipEnv(env, skip=config.skip_frame)
-    env = PreproWrapper(env, prepro=greyscale, shape=(80, 80, 1), 
-                        overwrite_render=config.overwrite_render)
+  # make env
+  env = gym.make(config.env_name)
+  env = MaxAndSkipEnv(env, skip=config.skip_frame)
+  env = PreproWrapper(env, prepro=greyscale, shape=(80, 80, 1), 
+            overwrite_render=config.overwrite_render)
 
-    # exploration strategy
-    exp_schedule = LinearExploration(env, config.eps_begin, 
-            config.eps_end, config.eps_nsteps)
+  # exploration strategy
+  exp_schedule = LinearExploration(env, config.eps_begin, 
+      config.eps_end, config.eps_nsteps)
 
-    # learning rate schedule
-    lr_schedule  = LinearSchedule(config.lr_begin, config.lr_end,
-            config.lr_nsteps)
+  # learning rate schedule
+  lr_schedule  = LinearSchedule(config.lr_begin, config.lr_end,
+      config.lr_nsteps)
 
-    # train model
-    model = Linear(env, config)
-    model.run(exp_schedule, lr_schedule)
+  # train model
+  model = Linear(env, config)
+  model.run(exp_schedule, lr_schedule)
